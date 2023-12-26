d5633615b4a5fe1d0f6b6bf6d2ea99f59f8febe2
salindae25 <salindae25@gmail.com>
2023-12-26 13:01:58 +0530

    added cmd to install additional npm dependecies

--- mix.exs
+++ mix.exs
@@ -63,7 +63,13 @@ defmodule GistClone.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: [
+        "deps.get",
+        "ecto.setup",
+        "assets.setup",
+        "assets.build",
+        "cmd npm install --perfix assets"
+      ],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
